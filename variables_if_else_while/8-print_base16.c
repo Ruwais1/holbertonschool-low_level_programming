ce060d0a6b502b431ceb5424f0b34ece920132c3
Your Name <you@example.com>
2026-02-27 15:42:04 +0000

    add 8 task

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,225 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	/* 1. طباعة الأرقام من 0 إلى 9 */
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+	}
+
+	/* 2. طباعة الحروف من a إلى f */
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+
+	/* 3. طباعة السطر الجديد */
+	putchar('\n');
+
+	return (0);
+}#include <stdio.h>
+
+/**
+ *  * main - prints all the numbers of base 16 in lowercase
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		int num;
+			char letter;
+
+				/* 1. طباعة الأرقام من 0 إلى 9 */
+				for (num = 0; num < 10; num++)
+				{
+							putchar(num + '0');
+				}
+
+					/* 2. طباعة الحروف من a إلى f */
+					for (letter = 'a'; letter <= 'f'; letter++)
+					{
+								putchar(letter);
+					}
+
+						/* 3. طباعة السطر الجديد */
+						putchar('\n');
+
+							return (0);
+}#include <stdio.h>
+
+/**
+ *  * main - prints all the numbers of base 16 in lowercase
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		int num;
+			char letter;
+
+				/* 1. طباعة الأرقام من 0 إلى 9 */
+				for (num = 0; num < 10; num++)
+				{
+							putchar(num + '0');
+				}
+
+					/* 2. طباعة الحروف من a إلى f */
+					for (letter = 'a'; letter <= 'f'; letter++)
+					{
+								putchar(letter);
+					}
+
+						/* 3. طباعة السطر الجديد */
+						putchar('\n');
+
+							return (0);
+}#include <stdio.h>
+
+/**
+ *  * main - prints all the numbers of base 16 in lowercase
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		int num;
+			char letter;
+
+				/* 1. طباعة الأرقام من 0 إلى 9 */
+				for (num = 0; num < 10; num++)
+				{
+							putchar(num + '0');
+				}
+
+					/* 2. طباعة الحروف من a إلى f */
+					for (letter = 'a'; letter <= 'f'; letter++)
+					{
+								putchar(letter);
+					}
+
+						/* 3. طباعة السطر الجديد */
+						putchar('\n');
+
+							return (0);
+}#include <stdio.h>
+
+/**
+ *  * main - prints all the numbers of base 16 in lowercase
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		int num;
+			char letter;
+
+				/* 1. طباعة الأرقام من 0 إلى 9 */
+				for (num = 0; num < 10; num++)
+				{
+							putchar(num + '0');
+				}
+
+					/* 2. طباعة الحروف من a إلى f */
+					for (letter = 'a'; letter <= 'f'; letter++)
+					{
+								putchar(letter);
+					}
+
+						/* 3. طباعة السطر الجديد */
+						putchar('\n');
+
+							return (0);
+}#include <stdio.h>
+
+/**
+ *  * main - prints all the numbers of base 16 in lowercase
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		int num;
+			char letter;
+
+				/* 1. طباعة الأرقام من 0 إلى 9 */
+				for (num = 0; num < 10; num++)
+				{
+							putchar(num + '0');
+				}
+
+					/* 2. طباعة الحروف من a إلى f */
+					for (letter = 'a'; letter <= 'f'; letter++)
+					{
+								putchar(letter);
+					}
+
+						/* 3. طباعة السطر الجديد */
+						putchar('\n');
+
+							return (0);
+}#include <stdio.h>
+
+/**
+ *  * main - prints all the numbers of base 16 in lowercase
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		int num;
+			char letter;
+
+				/* 1. طباعة الأرقام من 0 إلى 9 */
+				for (num = 0; num < 10; num++)
+				{
+							putchar(num + '0');
+				}
+
+					/* 2. طباعة الحروف من a إلى f */
+					for (letter = 'a'; letter <= 'f'; letter++)
+					{
+								putchar(letter);
+					}
+
+						/* 3. طباعة السطر الجديد */
+						putchar('\n');
+
+							return (0);
+}#include <stdio.h>
+
+/**
+ *  * main - prints all the numbers of base 16 in lowercase
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		int num;
+			char letter;
+
+				/* 1. طباعة الأرقام من 0 إلى 9 */
+				for (num = 0; num < 10; num++)
+				{
+							putchar(num + '0');
+				}
+
+					/* 2. طباعة الحروف من a إلى f */
+					for (letter = 'a'; letter <= 'f'; letter++)
+					{
+								putchar(letter);
+					}
+
+						/* 3. طباعة السطر الجديد */
+						putchar('\n');
+
+							return (0);
+}
