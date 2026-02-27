25bdd8757ef1c2b469838b101959fac547c60f1e
Your Name <you@example.com>
2026-02-27 16:32:27 +0000

    Add 2 task

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter); /* الاستخدام الأول */
+		}
+		_putchar('\n'); /* الاستخدام الثاني */
+	}
+}
