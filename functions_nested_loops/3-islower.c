ff548f7e244fd815c4f6ccd0074032161746dea9
Your Name <you@example.com>
2026-02-27 16:59:40 +0000

    a

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,13 +2,12 @@
 
 /**
  * _islower - checks for lowercase character
- * @c: the character to check (represented as an integer)
+ * @c: the character to check
  *
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
-	/* التحقق إذا كانت قيمة c تقع بين 'a' و 'z' */
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
---
+++ functions_nested_loops/main_0.c
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* نموذج دالة الطباعة الأساسية */
+int _putchar(char c);
+
+/* نموذج دالة التمرين رقم 3 */
+int _islower(int c);
+
+#endif
