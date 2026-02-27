3d0142916d85c189a2c5da19c6eac4de06eeb135
Your Name <you@example.com>
2026-02-27 18:00:03 +0000

    fix

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,16 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-/* ... الدوال السابقة ... */
-int add(int, int);
-int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int); /* النموذج الجديد */
-void jack_bauer(void);
-void times_table(void);
-void print_to_98(int n);
+/* تأكد من إضافة السطر أدناه */
+void print_times_table(int n);
 
 #endif
