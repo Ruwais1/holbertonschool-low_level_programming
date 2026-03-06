68d3a7de110044856f220d0efadaefb8452f06a8
root <liruwais1@gmail.com>
2026-03-06 04:35:05 +0000

    Task 4: Function that prints a string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	/* 1. حساب طول النص */
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	/* 2. الطباعة من الخلف للأمام */
+	for (i--; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
