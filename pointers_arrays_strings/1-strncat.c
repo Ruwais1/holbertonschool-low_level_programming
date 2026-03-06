47ae81f29b972aec4adee8414872cecbf50d4cf4
root <liruwais1@gmail.com>
2026-03-06 06:31:02 +0000

    Task 1: Fix Betty style line length

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,33 +1,26 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings using at most n bytes from src
+ * _strncat - concatenates two strings using n bytes
  * @dest: destination string
  * @src: source string
- * @n: limit of bytes to copy from src
+ * @n: limit of bytes to copy
  *
- * Return: pointer to the resulting string dest
+ * Return: pointer to dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-	int j = 0;
+	int i = 0, j = 0;
 
-	/* 1. الوصول لنهاية النص الأول dest */
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
 
-	/* 2. نسخ n حرف من src أو التوقف إذا وصلنا لنهاية src */
 	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
-
-	/* 3. إضافة علامة النهاية دائماً */
 	dest[i] = '\0';
 
 	return (dest);
