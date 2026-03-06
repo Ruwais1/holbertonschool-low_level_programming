396ab4eb4d257979a2f1630aa2a0bf75ff0d0c92
root <liruwais1@gmail.com>
2026-03-06 04:37:52 +0000

    Task 5: Fix Betty style line length warning

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,21 +8,17 @@
  */
 void rev_string(char *s)
 {
-	int len = 0;
-	int i = 0;
+	int len = 0, i = 0;
 	char temp;
 
-	/* 1. حساب طول النص */
 	while (s[len] != '\0')
-	{
 		len++;
-	}
 
-	/* 2. تبديل الحروف (الأول مع الأخير، الثاني مع قبل الأخير...) */
-	for (i = 0; i < len / 2; i++)
+	while (i < len--)
 	{
 		temp = s[i];
-		s[i] = s[len - 1 - i];
-		s[len - 1 - i] = temp;
+		s[i] = s[len];
+		s[len] = temp;
+		i++;
 	}
 }
