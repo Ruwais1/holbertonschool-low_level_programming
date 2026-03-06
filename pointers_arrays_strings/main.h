0479f8a925a786ad07bda52555ab06b40cd70e18
root <liruwais1@gmail.com>
2026-03-06 06:36:41 +0000

    add task 11

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,5 +15,6 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strncat(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
 
 #endif
