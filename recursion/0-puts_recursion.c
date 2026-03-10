49768b1a91c32f36ce615cf56d6c7dbc241b373e
Ruwais1 <liruwais1@gmail.com>
2026-03-10 20:51:48 +0000

    Task 0: _final the final the finale

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,23 +1,18 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: pointer to the string to be printed.
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to the string
  *
- * Return: void.
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-    /* Base Case: If we reach the null terminator '\0' */
-    if (*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
-
-    /* Print the current character pointed to by s */
-    _putchar(*s);
-
-    /* Recursive Call: Move to the next character (s + 1) */
-    _puts_recursion(s + 1);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
