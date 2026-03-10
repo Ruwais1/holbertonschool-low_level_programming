3fa10772444751d8f85b203fbd29a1b4f97c4911
Ruwais1 <liruwais1@gmail.com>
2026-03-10 22:21:04 +0000

    Task 7: is_palindrome

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string to calculate the length of
+ *
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_palindrome - checks recursively if a string is a palindrome
+ * @s: the string to check
+ * @start: the starting index
+ * @end: the ending index
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+
+	if (s[start] != s[end])
+		return (0);
+
+	return (check_palindrome(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: the string to check
+ *
+ * Return: 1 if it is a palindrome, 0 if it's not
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	if (len <= 1)
+		return (1);
+
+	return (check_palindrome(s, 0, len - 1));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -8,8 +8,8 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int find_sqrt(int n, int i);
 int is_prime_number(int n);
-int check_prime(int n, int i);
+int is_palindrome(char *s);
+int check_palindrome(char *s, int start, int end);
 
 #endif
