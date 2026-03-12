9237c09d4ce5793080dfe5c13818f85b36720d6f
Ruwais1 <liruwais1@gmail.com>
2026-03-12 17:28:26 +0000

    Update the final final task

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -10,27 +10,19 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	/* Base case: both strings reach the end */
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 
-	/* If the characters match, move both pointers forward */
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
-	/* If we hit a wildcard in s2 */
 	if (*s2 == '*')
 	{
-		/* If s1 is empty, our only option is to move s2 forward */
 		if (*s1 == '\0')
 			return (wildcmp(s1, s2 + 1));
-		
-		/* * Branch 1: Try skipping the character in s1 (wildcard matches it)
-		 * Branch 2: Try moving past the wildcard in s2 (wildcard matches empty)
-		 */
+
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
 
-	/* If characters don't match and there's no wildcard */
 	return (0);
 }
