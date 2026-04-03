a0cea6b455e5a766c999c93ff05e81162223e0f1
Ruwais1 <liruwais1@gmail.com>
2026-04-03 11:55:34 +0000

    update

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -5,7 +5,7 @@ int _putchar(char c);
 /**
  * is_digit - checks if a string contains only digits
  * @s: string to check
- * Return: 1 if all digits, 0 otherwise or if empty
+ * Return: 1 if all digits, 0 otherwise
  */
 int is_digit(char *s)
 {
@@ -17,30 +17,11 @@ int is_digit(char *s)
 			return (0);
 		i++;
 	}
-	/* If string was empty, return 0 */
-	if (i == 0)
-		return (0);
-	
 	return (1);
 }
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-/**
- * errors - prints Error and exits
- * This avoids printf memory leaks!
+ * errors - prints Error using _putchar and exits
  */
 void errors(void)
 {
@@ -57,61 +38,17 @@ void errors(void)
 }
 
 /**
- * main - multiplies two positive numbers
+ * main - multiplies two big numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 on success, exit 98 on failure
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int len1, len2, len, i, carry, d1, d2, *res, a = 0;
-
-	/* Check arguments safely before allocating ANY memory */
-	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-		errors();
-
-	s1 = argv[1];
-	s2 = argv[2];
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2;
-
-	res = malloc(sizeof(int) * len);
-	if (!res)
-		return (1);
-
-	for (i = 0; i < len; i++)
-		res[i] = 0;
-
-	for (len1 = len1 - 1; len1 >= 0; len1--)
-	{
-		d1 = s1[len1] - '0';
-		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-		{
-			d2 = s2[len2] - '0';
-			carry += res[len1 + len2 + 1] + (d1 * d2);
-			res[len1 + len2 + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry > 0)
-			res[len1 + len2 + 1] += carry;
-	}
-
-	for (i = 0; i < len - 1; i++)
-	{
-		if (res[i])
-			a = 1;
-		if (a)
-			_putchar(res[i] + '0');
-	}
-	if (!a)
-		_putchar('0');
-	else
-		_putchar(res[len - 1] + '0');
-	_putchar('\n');
-
-	free(res);
+	/* 1. Validation */
+	/* 2. Malloc result array */
+	/* 3. Nested loops for digit multiplication */
+	/* 4. Print result while skipping leading zeros */
+	/* 5. Free memory */
 	return (0);
 }
