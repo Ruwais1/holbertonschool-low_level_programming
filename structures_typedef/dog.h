c9cb629ecab9cd481546684c35a7d5bc00da6031
Ruwais1 <liruwais1@gmail.com>
2026-04-03 11:51:45 +0000

    5

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,6 +6,8 @@
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Owner of the dog
+ *
+ * Description: Structure that stores name, age and owner of a dog.
  */
 struct dog
 {
@@ -19,9 +21,18 @@ struct dog
  */
 typedef struct dog dog_t;
 
-/* Prototypes for all functions */
+/* --- Function Prototypes --- */
+
+/* Task 1: Initialize a dog struct */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/* Task 2: Print a dog struct */
 void print_dog(struct dog *d);
+
+/* Task 4: Create a new dog (with memory allocation) */
 dog_t *new_dog(char *name, float age, char *owner);
+
+/* Task 5: Free memory allocated for a dog */
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
