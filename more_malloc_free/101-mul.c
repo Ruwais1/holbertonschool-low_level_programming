43bd228403699841777c1c692e10b6a272cfc601
Ruwais1 <liruwais1@gmail.com>
2026-04-03 07:53:30 +0000

    final update

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -6,7 +6,7 @@ int _putchar(char c);
 /**
  * is_digit - checks if a string contains only digits
  * @s: string to check
- * Return: 0 if not digits, 1 if all digits
+ * Return: 1 if all digits, 0 otherwise
  */
 int is_digit(char *s)
 {
@@ -39,40 +39,47 @@ int _strlen(char *s)
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: always 0 (Success)
+ * Return: 0 on success, exit 98 on failure
  */
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *res, a = 0;
+	int len1, len2, len, i, carry, d1, d2, *res, a = 0;
 
+	/* 1. Check arguments BEFORE malloc to avoid leaks */
 	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	s1 = argv[1], s2 = argv[2];
-	len1 = _strlen(s1), len2 = _strlen(s2);
+
+	s1 = argv[1];
+	s2 = argv[2];
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 	len = len1 + len2;
 	res = malloc(sizeof(int) * len);
 	if (!res)
 		return (1);
+
 	for (i = 0; i < len; i++)
 		res[i] = 0;
+
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = s1[len1] - '0';
+		d1 = s1[len1] - '0';
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			digit2 = s2[len2] - '0';
-			carry += res[len1 + len2 + 1] + (digit1 * digit2);
+			d2 = s2[len2] - '0';
+			carry += res[len1 + len2 + 1] + (d1 * d2);
 			res[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
 			res[len1 + len2 + 1] += carry;
 	}
+
 	for (i = 0; i < len - 1; i++)
 	{
 		if (res[i])
@@ -85,6 +92,7 @@ int main(int argc, char *argv[])
 	else
 		_putchar(res[len - 1] + '0');
 	_putchar('\n');
+
 	free(res);
 	return (0);
 }
