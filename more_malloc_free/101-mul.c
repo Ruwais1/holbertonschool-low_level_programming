920800e0a5edeae6681c38a696b85aaa0fa60d23
Ruwais1 <liruwais1@gmail.com>
2026-04-03 11:57:16 +0000

    u

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -21,16 +21,30 @@ int is_digit(char *s)
 }
 
 /**
- * errors - prints Error using _putchar and exits
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * errors - prints Error and exits with 98
  */
 void errors(void)
 {
-	char *err = "Error";
+	char *s = "Error";
 	int i = 0;
 
-	while (err[i])
+	while (s[i])
 	{
-		_putchar(err[i]);
+		_putchar(s[i]);
 		i++;
 	}
 	_putchar('\n');
@@ -38,17 +52,56 @@ void errors(void)
 }
 
 /**
- * main - multiplies two big numbers
+ * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	/* 1. Validation */
-	/* 2. Malloc result array */
-	/* 3. Nested loops for digit multiplication */
-	/* 4. Print result while skipping leading zeros */
-	/* 5. Free memory */
+	char *s1, *s2;
+	int len1, len2, len, i, carry, d1, d2, *res, a = 0;
+
+	/* Using argc and argv here fixes the "unused parameter" error */
+	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+		errors();
+
+	s1 = argv[1];
+	s2 = argv[2];
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2;
+	res = malloc(sizeof(int) * len);
+	if (!res)
+		return (1);
+	for (i = 0; i < len; i++)
+		res[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		d1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			d2 = s2[len2] - '0';
+			carry += res[len1 + len2 + 1] + (d1 * d2);
+			res[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			res[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (res[i])
+			a = 1;
+		if (a)
+			_putchar(res[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	else
+		_putchar(res[len - 1] + '0');
+	_putchar('\n');
+	free(res);
 	return (0);
 }
