5f8160833d3a03b8fccf0eb6de703c2e0d853b86
Ruwais1 <liruwais1@gmail.com>
2026-04-03 12:00:41 +0000

    final

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -3,6 +3,19 @@
 int _putchar(char c);
 
 /**
+ * _print_error - prints "Error" followed by a newline and exits
+ */
+void _print_error(void)
+{
+	char *err = "Error\n";
+	int i;
+
+	for (i = 0; err[i]; i++)
+		_putchar(err[i]);
+	exit(98);
+}
+
+/**
  * is_digit - checks if a string contains only digits
  * @s: string to check
  * Return: 1 if all digits, 0 otherwise
@@ -11,6 +24,8 @@ int is_digit(char *s)
 {
 	int i = 0;
 
+	if (!s || s[i] == '\0')
+		return (0);
 	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
@@ -23,72 +38,53 @@ int is_digit(char *s)
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
- * Return: the length of the string
+ * Return: the length
  */
 int _strlen(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i])
 		i++;
 	return (i);
 }
 
 /**
- * errors - prints Error and exits with 98
- */
-void errors(void)
-{
-	char *s = "Error";
-	int i = 0;
-
-	while (s[i])
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar('\n');
-	exit(98);
-}
-
-/**
  * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, carry, d1, d2, *res, a = 0;
+	int len1, len2, len, i, j, carry, d1, d2, *res, a = 0;
 
-	/* Using argc and argv here fixes the "unused parameter" error */
 	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-		errors();
+		_print_error();
 
-	s1 = argv[1];
-	s2 = argv[2];
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
+	s1 = argv[1], s2 = argv[2];
+	len1 = _strlen(s1), len2 = _strlen(s2);
 	len = len1 + len2;
 	res = malloc(sizeof(int) * len);
 	if (!res)
 		return (1);
 	for (i = 0; i < len; i++)
 		res[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		d1 = s1[len1] - '0';
+		d1 = s1[i] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		for (j = len2 - 1; j >= 0; j--)
 		{
-			d2 = s2[len2] - '0';
-			carry += res[len1 + len2 + 1] + (d1 * d2);
-			res[len1 + len2 + 1] = carry % 10;
+			d2 = s2[j] - '0';
+			carry += res[i + j + 1] + (d1 * d2);
+			res[i + j + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-			res[len1 + len2 + 1] += carry;
+			res[i + j + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
 	{
@@ -97,10 +93,7 @@ int main(int argc, char *argv[])
 		if (a)
 			_putchar(res[i] + '0');
 	}
-	if (!a)
-		_putchar('0');
-	else
-		_putchar(res[len - 1] + '0');
+	_putchar(res[len - 1] + '0');
 	_putchar('\n');
 	free(res);
 	return (0);
