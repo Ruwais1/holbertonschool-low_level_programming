e3e4c48fd8c31a10510153d086e33d08f8cec93a
Ruwais1 <liruwais1@gmail.com>
2026-04-05 15:03:16 +0000

    update

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,17 +1,7 @@
-#include "function_pointers.h"
-#include <stdlib.h>
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-/**
- * print_name - prints a name using a function pointer
- * @name: name of the person to be printed
- * @f: pointer to the function that defines the printing format
- *
- * Return: Nothing.
- */
-void print_name(char *name, void (*f)(char *))
-{
-	if (name != NULL && f != NULL)
-	{
-		f(name);
-	}
-}
+/* Prototype of the function */
+void print_name(char *name, void (*f)(char *));
+
+#endif
