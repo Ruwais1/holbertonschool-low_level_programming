cbbaa8edfb57d428cea3e9770bc71d3930572625
Ruwais1 <liruwais1@gmail.com>
2026-04-05 15:02:08 +0000

    update

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,18 +2,16 @@
 #include <stdlib.h>
 
 /**
- * print_name - prints a name
- * @name: name of the person
- * @f: pointer to the printing function
+ * print_name - prints a name using a function pointer
+ * @name: name of the person to be printed
+ * @f: pointer to the function that defines the printing format
  *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-    /*  NULL */
-    if (name != NULL && f != NULL)
-    {
-        /*� name لها */
-        f(name);
-    }
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
