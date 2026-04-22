ea70ad9cd5d8d76a42626cd84634742ef122f81d
Ruwais1 <liruwais1@gmail.com>
2026-04-22 17:06:01 +0000

    update

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,24 +11,16 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	list_t *temp;
+	list_t *new_node, *temp;
 	unsigned int len = 0;
 
-	/* Check if the input string is NULL */
 	if (str == NULL)
 		return (NULL);
 
-	/* Calculate the length of the string */
-	while (str[len])
-		len++;
-
-	/* Allocate memory for the new node */
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	/* Duplicate the string and store it in the node */
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
@@ -36,25 +28,22 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	/* Initialize the new node */
-	new_node->len = len;
-	new_node->next = NULL; /* Because it will be the last node */
+	while (str[len])
+		len++;
 
-	/* If the list is empty, make the new node the head */
+	new_node->len = len;
+	new_node->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
 
-	/* Otherwise, traverse the list to find the last node */
 	temp = *head;
 	while (temp->next != NULL)
-	{
 		temp = temp->next;
-	}
 
-	/* Attach the new node at the end */
 	temp->next = new_node;
 
 	return (new_node);
