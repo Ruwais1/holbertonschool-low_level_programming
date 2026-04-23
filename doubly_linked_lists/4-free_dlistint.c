ade40da4fcc80aa6356af0f389495544d3f15966
Ruwais1 <liruwais1@gmail.com>
2026-04-23 22:45:40 +0000

    fixing task 4

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -11,16 +11,10 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
 
-	/* Loop through the list until the end */
 	while (head != NULL)
 	{
-		/* Save the current node */
 		temp = head;
-		
-		/* Move head to the next node */
 		head = head->next;
-		
-		/* Free the saved node */
 		free(temp);
 	}
 }
